180a1c73dd0bb7d71285d19d2681137875537b53
Jerome HOUDAN <jerom.houdan@gmail.com>
2016-03-15 16:52:06 +0100

    bunch of fixs

--- backup_jh/test.go
+++ backup_jh/test.go
@@ -3,61 +3,24 @@ package main
 import (
 	"os"
 	"fmt"
-	"os/exec"
 	"io/ioutil"
-	"strings"
-	"encoding/json"
+	"vogshpere.42.fr/taskmaster.git/backup_nathan/tmconf"
 )
 
-type Process struct {
-	Name			string `json:Name`
-	Cmd				string `json:cmd`
-	Numprocs		int `json:numprocs`
-	Umask			int `json:umask`
-	Workingdir		string `json:workingdir`
-	Autostart		bool `json:autostart`
-	Autorestart		string `json:autorestart`
-	Exitcodes		[]int `json:exitcodes`
-	Startretries	int `json:startretries`
-	Starttime		int `json:starttime`
-	Stoptime		int `json:stoptime`
-	Stdout			string `json:stdout`
-	Stderr			string `json:stderr`
-}
-
-type Container struct {
-	Programs []Process
-}
-
 func main() {
-	var container Container
 	if len(os.Args) == 1 {
 		fmt.Fprintf(os.Stderr, "taskmaster: requires a config file\n")
 		return
 	}
 	config_file := os.Args[1]
-	data := parseFile(config_file)
-	err := json.Unmarshal(data, &container)
+	container, err := tmconf.ReadConfig(config_file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "critical error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "taskmaster: %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Println(string(data))
-	for i := 0; i < len(container.Programs); i++ {
-		fmt.Println(container.Programs[i])
-	}
-	//Test execution
-	for _, v := range container.Programs {
-		tmp := strings.Split(v.Cmd, " ")
-		fmt.Println(tmp)
-		cmd := exec.Command(tmp[0], tmp[1:]...)
-		result, err := cmd.Output()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
-		} else {
-			fmt.Println("Cmd successfully launched:")
-			fmt.Println(string(result))
-		}
+	fmt.Println(len(container), "procs found\n")
+	for _, v := range container {
+		fmt.Println(v)
 	}
 }
 
@@ -68,12 +31,4 @@ func parseFile(filename string) []byte {
 		os.Exit(1)
 	}
 	return data
-}
-
-func (p Process) String() string {
-	str := fmt.Sprintf("Name: %s -> cmd %s\n", p.Name, p.Cmd)
-	str += fmt.Sprintf("\tNProcs: %d || Umask: %d\n", p.Numprocs, p.Umask)
-	str += fmt.Sprintf("\tOut: %s || Err: %s\n", p.Stdout, p.Stderr)
-	str += fmt.Sprintf("\tWDir: %s || Autorestart: %t\n", p.Workingdir, p.Autostart)
-	return str
 }
