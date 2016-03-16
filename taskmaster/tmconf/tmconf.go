c0c74e3f7fc05093288147a2294b9fb5087ba1c5
Jerome HOUDAN <jerom.houdan@gmail.com>
2016-03-16 16:16:44 +0100

    taskmaster: the Real One

---
+++ taskmaster/tmconf/tmconf.go
@@ -0,0 +1,40 @@
+package tmconf
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type ProcSettings struct {
+	Name         string
+	Cmd          string
+	Numprocs     int
+	WorkingDir	 string
+	Autostart    bool
+	Autorestart  string
+	Exitcodes    []int
+	Startretries int
+	Starttime    int
+	Stopsignal   string
+	Stoptime     int
+	Stdout       string
+	Stderr       string
+	Env          []string
+}
+
+func (s ProcSettings) String() string {
+	return fmt.Sprintf("{Name: %s\nCmd: %s\nNumprocs: %d\nAutostart: %v\nAutorestart: %s\nExitcode: %v\nStartretries: %d\nStarttime: %d\nStopsignal: %s\nStoptime: %d\nStdout: %s\nStderr: %s\nEnv: %v}\n",
+		s.Name, s.Cmd, s.Numprocs, s.Autostart, s.Autorestart, s.Exitcodes, s.Startretries,
+		s.Starttime, s.Stopsignal, s.Stoptime, s.Stdout, s.Stderr, s.Env)
+}
+
+func ReadConfig(filename string) (settings []ProcSettings, err error) {
+	var conf []ProcSettings
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &conf)
+	return conf, err
+}
